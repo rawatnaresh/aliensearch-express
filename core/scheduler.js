a2761d72c9e855b35f533c8147278c950c3ee0e9
Naresh Rawat <nareshrawat33@gmail.com>
2018-10-29 14:05:24 +0530

    time changed

--- core/scheduler.js
+++ core/scheduler.js
@@ -4,7 +4,7 @@ const Site = require('../models/Site');
 const crawler = require('./crawler');
 
 module.exports.crawlerCronJob =  new CronJob({
-        cronTime:'0 03 14 * * *',
+        cronTime:'0 07 14 * * *',
         onTick:function(){
             console.log("-------------------------Crawling Started--------------------");
     
